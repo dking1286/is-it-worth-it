a4d6523e0e228b9785cf95e20fdefca6adb88b3a
Dlaosb <Dlaosb@gmail.com>
2016-08-18 18:19:13 -0700

    Full functionality Is It Worth it Chrome Extensions

--- content.js
+++ content.js
@@ -1,82 +1,97 @@
 
-setInterval(function() {
-var elements = document.getElementsByTagName('*');
+//document.addEventListener("DOMContentLoaded", function() {
 
-chrome.storage.sync.get('salary', function(data) {
-	const salary = data.salary;
+var count = 0;
+function stop(running) {
 
-	for (var i = 0; i < elements.length; i++) {
-    var element = elements[i];
+  if (count === 1000) {
+    console.log("I STOPPED")
+    clearInterval(running);
 
-    for (var j = 0; j < element.childNodes.length; j++) {
-      var node = element.childNodes[j];
+  }
 
-
-    	if (node.nodeType === 3) {
-    		console.log(node);
-    		var regexTest = /\$\d+(?:\.\d+)?/gi;
-      	var text = node.nodeValue;
-
-     		var numberStrings = text.match(regexTest);
-
-     		if(numberStrings !== null) {
-     		console.log(numberStrings);
-     		numberStrings.forEach(function(numberString) {
-     			var numPrice = parseFloat(numberString.replace(/./, ''));
-   
-	     		var hours = convert(numPrice, salary);
-
-	     		//replace text matches entire string 
+}
 
 
-	     		var replacedText = text.replace(numberString, `${round(hours, 2)} hours of your life`); 
+var running = setInterval(function() {
 
-	     		console.log("hours", hours);
-	     		console.log("BREAK");
-	     		console.log("replaced text", replacedText);
-	      	//var replacedText = text.replace(/\$\d+(?:\.\d+)?/gi, 'ONE MILLION DOLLARS');
-	     
+  stop(running); 
+  count++;
+  console.log(count);
 
-	      	if (replacedText !== text) {
-	          element.replaceChild(document.createTextNode(replacedText), node);
-	        }
+  var elements = document.getElementsByTagName('*');
 
-     		});
+  chrome.storage.sync.get('salary', function(data) {
+    const salary = data.salary;
+
+    for (var i = 0; i < elements.length; i++) {
+      var element = elements[i];
+
+      for (var j = 0; j < element.childNodes.length; j++) {
+        var node = element.childNodes[j];
 
 
+        if (node.nodeType === 3) {
+          var regexTest = /\$\d+(?:\.\d+)?/gi;
+          var text = node.nodeValue;
 
-  	  }
+          var numberStrings = text.match(regexTest);
+
+          if (numberStrings !== null) {
+            numberStrings.forEach(function(numberString) {
+              var numPrice = parseFloat(numberString.replace(/./, ''));
+
+              var hours = convert(numPrice, salary);
+
+              //replace text matches entire string 
+
+
+              var replacedText = text.replace(numberString, `${round(hours, 2)} HRs of your Life`);
+              //var replacedText = text.replace(/\$\d+(?:\.\d+)?/gi, 'ONE MILLION DOLLARS');
+
+              if (replacedText !== text) {
+
+                //let newNode = document.createTextNod
+
+                element.replaceChild(document.createTextNode(replacedText), node);
+
+
+                //node = newNode; 
+
+
+              }
+
+            });
+          }
+        }
       }
     }
-	}
-});
+  });
+}, 1000);
 
 
-}, 10000);
 
 //match returns an array of matches 
 
 //convert returns cost of hours of life 
 function convert(price, salary) {
-	//assuming price has been converted from a string to a number 
-	let hourlyPay = salary / 2087;
-	let hoursOfLife = price / hourlyPay; 
-	return hoursOfLife; 
+  //assuming price has been converted from a string to a number 
+  let hourlyPay = salary / 2087;
+  let hoursOfLife = price / hourlyPay;
+  return hoursOfLife;
 
 }
 
 function round(value, decimals) {
-	return Number(Math.round(value+'e'+decimals)+'e-'+decimals);
+  return Number(Math.round(value + 'e' + decimals) + 'e-' + decimals);
 }
 
 // const regexTest = /\$\d+(?:\.\d+)?/gi;
 // const newMessage = 'one million dollars';
 // setInterval(function(){ 
-// 	console.log("BEFORE");
-// 	var y = document.body.innerHTML.replace(regexTest, newMessage);
-// 	console.log(y);
-// 	document.body.innerHTML = document.body.innerHTML.replace(regexTest, newMessage);
-// 	console.log("AFTER");
-// }, 3000); 
-
-
+//  console.log("BEFORE");
+//  var y = document.body.innerHTML.replace(regexTest, newMessage);
+//  console.log(y);
+//  document.body.innerHTML = document.body.innerHTML.replace(regexTest, newMessage);
+//  console.log("AFTER");
+// }, 3000);
--- popup.js
+++ popup.js
@@ -2,43 +2,99 @@
 
 document.addEventListener("DOMContentLoaded", function() {
 
-  var input = document.createElement('input'); 
-  input.setAttribute('type', 'text');
-  input.setAttribute('placeholder', 'Enter Salary');
-  input.setAttribute('id', 'salary'); 
+		var header = document.createElement('header');
+		header.setAttribute('id', 'header');
+		header.innerHTML = "Is It Worth It?"
+
+		var salaryInput = document.createElement('input');
+		salaryInput.setAttribute('type', 'text');
+		salaryInput.setAttribute('placeholder', 'Enter Salary');
+		salaryInput.setAttribute('id', 'salary');
+
+		var or = document.createElement('p');
+		or.setAttribute('id', 'or');
+		or.innerHTML = "-or-"
+
+		var hourInput = document.createElement('input');
+		hourInput.setAttribute('type', 'text');
+		hourInput.setAttribute('placeholder', 'Enter Hourly Rate');
+		hourInput.setAttribute('id', 'hourRate');
+
+		var submit = document.createElement('input');
+		submit.setAttribute('id', 'submitBtn');
+		submit.setAttribute('type', 'submit');
+		submit.setAttribute('value', "submit");
+
+		var showSalary = document.createElement("p");
+		showSalary.setAttribute('id', 'salary-display');
+		//showSalary.innerHTML = "Current Hourly Rate";
+
+		chrome.storage.sync.get('salary', function(data) {
+
+				//added the converToHourly function and passed in data.salary 
+				showSalary.innerHTML = `Current Hourly Rate: $${round(convertToHourly(data.salary), 2)}`;
+				console.log('Retrived salary ' + data.salary);
+		});
+
+		document.body.appendChild(header);
+		document.body.appendChild(salaryInput);
+		document.body.appendChild(or);
+		document.body.appendChild(hourInput);
+		document.body.appendChild(submit);
+		document.body.appendChild(showSalary);
 
 
-  var submit = document.createElement('input');
-  submit.setAttribute('id', 'submitBtn');
-  submit.setAttribute('type', 'submit');
-  submit.setAttribute('value', "submit");
+		function myListener(event) {
+				var salary = salaryInput.value;
+				var hourly = hourInput.value;
+				//var salary = input.value; 
+				//showSalary.innerHTML = salary;
+				//input.value = '';
+				//storage in some location in memory 
+				
+				// chrome.storage.sync.set({ 'salary': salary }, function() {
+				// 		console.log("salary", salary);
+				// });
 
-  var showSalary = document.createElement("p");
-  showSalary.setAttribute('id', 'salary-display');
+				if (salary && hourly) {
+						showSalary.innerHTML = "Enter 1 Value Please";
+						return showSalary.innerHTML;
 
-  chrome.storage.sync.get('salary', function(data) {
-    showSalary.innerHTML = data.salary;
-    console.log('Retrived salary ' + data.salary);
-  });
+				} else if (!salary && !hourly) {
+						showSalary.innerHTML = "Enter 1 Value Please";
+						return showSalary.innerHTML;
+				} else {
+						if (salary) {
+							chrome.storage.sync.set({ 'salary': salary }, function() {
+							console.log("salary", salary);
+							});
+								showSalary.innerHTML = `Current Hourly Rate: $${round(convertToHourly(salary), 2)}`;
+								return showSalary.innerHTML;
+						} else {
+							chrome.storage.sync.set({ 'salary': convertToSalary(hourly) }, function() {
+							console.log("salary", convertToSalary(hourly));
+							});
+								showSalary.innerHTML = `Current Hourly Rate: $${hourly}`;
+								return showSalary.innerHTML;
+						}
+				}
+		}
 
-  document.body.appendChild(input);
-  document.body.appendChild(submit);
-  document.body.appendChild(showSalary);
-
-
-  function myListener(event) {
-    var salary = input.value; 
-    showSalary.innerHTML = salary;
-    input.value = '';
-    //storage in some location in memory 
-    chrome.storage.sync.set({'salary': salary}, function() {
-      console.log("salary", salary); 
-    });
-  }
-
-
-
-  submit.addEventListener('click', myListener);
-
+		submit.addEventListener('click', myListener);
 
 });
+
+function round(value, decimals) {
+  return Number(Math.round(value + 'e' + decimals) + 'e-' + decimals);
+}
+
+function convertToHourly(salary) {
+		//assuming price has been converted from a string to a number 
+		let hourlyRate = salary / 2087;
+		return hourlyRate;
+}
+
+function convertToSalary(hourlyRate) {
+	let salary = 2087 * hourlyRate;
+	return salary; 
+}
